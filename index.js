ff1ce8ec53b345809f7bcc464f04a26817cbbf7c
TazwoarCommits <tazwoarlives@gmail.com>
2025-01-27 02:36:54 +0600

    recheck

--- index.js
+++ index.js
@@ -45,22 +45,22 @@ async function run() {
 
         //  verify token
 
-         const verifyTOken = (req , res , next) => {
-            console.log(req.headers.authorization) ; 
-            if(!req.headers.authorization){
-                return res.status(401).send({message : "Unauthorized Access"});
-            }
-            const token = req.headers.authorization.split(" ")[1] ;
-            console.log(token)
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err,decoded) => {
-                if(err){
-                    return res.status(401).send({message : "Unauthorized Access" })
-                }
-                req.decoded = decoded ;
-                next() ;
-            }) 
+        //  const verifyTOken = (req , res , next) => {
+        //     console.log(req.headers.authorization) ; 
+        //     if(!req.headers.authorization){
+        //         return res.status(401).send({message : "Unauthorized Access"});
+        //     }
+        //     const token = req.headers.authorization.split(" ")[1] ;
+        //     console.log(token)
+        //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err,decoded) => {
+        //         if(err){
+        //             return res.status(401).send({message : "Unauthorized Access" })
+        //         }
+        //         req.decoded = decoded ;
+        //         next() ;
+        //     }) 
           
-        } ;
+        // } ;
 
 
 
